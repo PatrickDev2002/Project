cb6bebe397b4837883e244cc9da6ca3771d34da3
Patrick <patrick@fewlines.co>
2021-06-15 14:18:49 +0200

    save1

--- pages/posts/abdt-tuto.js
+++ pages/posts/abdt-tuto.js
@@ -7,11 +7,36 @@ return (
       <head>
           <title>Piano tutorial</title>
       </head>
-          <h1>Piano piece - Au bord du temps (Keyboard version)</h1>
-              <div className={utilStyles.firstSkill}>
-                  <video controls width="500" height="300">
+          <h1>Piano piece - Au bord du temps (Tuto video part)</h1>
+          <h2 className={utilStyles.twoFirstSkillsFont}>
+            <p>
+            (G) + (K) + (O), alternate with (^)
+            </p>
+            <p>
+            (F) + (N) + (+) + (6), alternate with (9)
+            </p>
+          </h2>
+              <div className={utilStyles.twoFirstSkills}>
+                  <video className={utilStyles.videoStyles} controls width="500" height="300">
                       <source src='/images/ABDT-1st.mov' type="video/mp4"/>
                   </video>
+                  <video className={utilStyles.videoStyles} controls width="500" height="300">
+                      <source src='/images/ABDT-2nd.mov' type="video/mp4"/>
+                  </video>
+              </div>
+          <h3 className={utilStyles.twoLastSkillsFont}>
+            <p>
+            (R) + (G) + (^) + (+), alternate with the (7)</p>
+            <p>
+            (Y) + (N) + (+), alternate with the (I)</p>
+          </h3>
+              <div className={utilStyles.twoLastSkills}>
+                <video className={utilStyles.videoStyles} controls width="500" height="300">
+                  <source src='/images/ABDT-3rd.mov' type="video/mp4"/>
+                </video>
+                <video className={utilStyles.videoStyles} controls width="500" height="300">
+                  <source src='/images/ABDT-4th.mov' type="video/mp4"/>
+                </video>
               </div>
           <h3>
               <a href="/posts/first-post">
