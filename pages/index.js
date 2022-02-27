89d0e51b3cb6bffe5e6adc7a65a82ef4ede22387
PatrickDev2002 <patrick@fewlines.co>
2022-02-27 21:20:14 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
-import Head from 'next/head'
-import Layout, { siteTitle } from '../components/layout'
-import utilStyles from '../styles/utils.module.css'
+import Head from "next/head";
+import Layout, { siteTitle } from "../components/layout";
+import utilStyles from "../styles/utils.module.css";
 
 export default function Home() {
   return (
@@ -8,7 +8,9 @@ export default function Home() {
       <Head>
         <title>{siteTitle}</title>
       </Head>
-      <div className="background">  {/*lorsque que la className est entre guillemets, cela provient de styles/... */}
+      <div className="background">
+        {" "}
+        {/*lorsque que la className est entre guillemets, cela provient de styles/... */}
         <span></span>
         <span></span>
         <span></span>
@@ -35,22 +37,24 @@ export default function Home() {
       <section className={utilStyles.headingMd}>
         <p>[Voila un bon petit blog]</p>
         <p>
-          (This is a sample website - you’ll be building a site like this on :{' '}
+          (This is a sample website - you’ll be building a site like this on :{" "}
           <a href="https://nextjs.org/learn">Our next.js tutorial</a>.)
         </p>
         <p>
           If fou want to learn some piano skill, but you just have a keyboard :
           <a href="https://www.musicca.com/fr/piano">Play here.</a>
           <p>
-            Even if you don't know what playing, i made this little tuto for you :
-            (Choose your operating system for optimal playable skills)
+            Even if you don't know what playing, i made this little tuto for you
+            : (Choose your operating system for optimal playable skills)
             <div className={utilStyles.linkStyle}>
-            <a href="http://localhost:3000/posts/first-postMac">Learn Mac version.</a>
-            <a href="http://localhost:3000/posts/first-postWindows">Learn Windows version.</a>
+              <a href="../posts/first-postMac">Learn Mac version.</a>
+              <a href="http://localhost:3000/posts/first-postWindows">
+                Learn Windows version.
+              </a>
             </div>
           </p>
-          </p>
+        </p>
       </section>
     </Layout>
-  )
+  );
 }
