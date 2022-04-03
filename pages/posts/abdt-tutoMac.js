fec52d73b55aa1310107bfe82692f8b4ed85ec1a
PatrickDev2002 <patrick@fewlines.co>
2022-04-03 12:57:14 +0200

    Update abdt-tutoMac.js

--- pages/posts/abdt-tutoMac.js
+++ pages/posts/abdt-tutoMac.js
@@ -1,49 +1,61 @@
-import utilStyles from '../../styles/utils.module.css'
+import utilStyles from "../../styles/utils.module.css";
 
 export default function AbdtTuto() {
-return (
-  <section className={utilStyles.abdtTutoStyle}>
+  return (
+    <section className={utilStyles.abdtTutoStyle}>
       <div className={utilStyles.abdtTutoBg}></div>
       <head>
-          <title>Piano tutorial</title>
+        <title>Piano tutorial</title>
       </head>
-          <h1>Piano tuto - Au bord du temps (Video part)</h1>
-          <div className={utilStyles.abdtTitle}>
-            Let's see the rhytm of those skills
-          </div>
-          <h2 className={utilStyles.twoFirstSkillsFont}>
-            <p>
-            (G) + (K) + (O), alternate with (^)
-            </p>
-            <p>
-            (F) + (N) + (+) + (6), alternate with (9)
-            </p>
-          </h2>
-              <div className={utilStyles.twoFirstSkills}>
-                  <video className={utilStyles.videoStyles} controls width="500" height="300">
-                      <source src='/images/ABDT-1st.mov' type="video/mp4"/>
-                  </video>
-                  <video className={utilStyles.videoStyles} controls width="500" height="300">
-                      <source src='/images/ABDT-2nd.mov' type="video/mp4"/>
-                  </video>
-              </div>
-          <h3 className={utilStyles.twoLastSkillsFont}>
-            <p>
-            (R) + (G) + (^) + (+), alternate with the (7)</p>
-            <p>
-            (Y) + (N) + (+), alternate with the (I)</p>
-          </h3>
-              <div className={utilStyles.twoLastSkills}>
-                <video className={utilStyles.videoStyles} controls width="500" height="300">
-                  <source src='/images/ABDT-3rd.mov' type="video/mp4"/>
-                </video>
-                <video className={utilStyles.videoStyles} controls width="500" height="300">
-                  <source src='/images/ABDT-4th.mov' type="video/mp4"/>
-                </video>
-              </div>
-              <a href="/posts/first-postMac">
-              ← Back to the tuto
-              </a>
-  </section>
-      )
-  }
+      <h1>Piano tuto - Au bord du temps (Video part)</h1>
+      <div className={utilStyles.abdtTitle}>
+        Let's see the rhytm of those skills
+      </div>
+      <h2 className={utilStyles.twoFirstSkillsFont}>
+        <p>(G) + (K) + (O), alternate with (^)</p>
+        <p>(F) + (N) + (+) + (6), alternate with (9)</p>
+      </h2>
+      <div className={utilStyles.twoFirstSkills}>
+        <video
+          className={utilStyles.videoStyles}
+          controls
+          width="500"
+          height="300"
+        >
+          <source src="/images/ABDT-1st.mov" type="video/mp4" />
+        </video>
+        <video
+          className={utilStyles.videoStyles}
+          controls
+          width="500"
+          height="300"
+        >
+          <source src="/images/ABDT-2nd.mov" type="video/mp4" />
+        </video>
+      </div>
+      <h3 className={utilStyles.twoLastSkillsFont}>
+        <p>(R) + (G) + (^) + (+), alternate with the (7)</p>
+        <p>(Y) + (N) + (+), alternate with the (I)</p>
+      </h3>
+      <div className={utilStyles.twoLastSkills}>
+        <video
+          className={utilStyles.videoStyles}
+          controls
+          width="500"
+          height="300"
+        >
+          <source src="/images/ABDT-3rd.mov" type="video/mp4" />
+        </video>
+        <video
+          className={utilStyles.videoStyles}
+          controls
+          width="500"
+          height="300"
+        >
+          <source src="/images/ABDT-4th.mov" type="video/mp4" />
+        </video>
+      </div>
+      <a href="/posts/first-postMac">← Back to the tuto</a>
+    </section>
+  );
+}
